ef6d5af4b4023d0fe756df86e9e7cc2e6fc4712c
russtone <russtone@yandex.ru>
2015-08-12 15:30:51 +0300

    Added vi-mode indicator.

--- theme.zsh
+++ theme.zsh
@@ -1,3 +1,4 @@
+# turns path like '/home/user/foo/bar/baz' into '~/f/b/baz'
 function prompt_russtone_pwd {
   local pwd="${PWD/#$HOME/~}"
 
@@ -33,11 +34,7 @@ function prompt_russtone_human_time {
   echo "${seconds}s"
 }
 
-function prompt_russtone_preexec {
-  # Save timestamp before executing command
-  _prompt_russtone_timestamp_start=$(date +%s)
-}
-
+# Calc elapsed time and save it to _prompt_russtone_elapsed_time (if > 5s)
 function prompt_russtone_elapsed_time {
   local stop=$(date +%s)
   local start=${_prompt_russtone_timestamp_start:-$stop}
@@ -50,16 +47,28 @@ function prompt_russtone_elapsed_time {
   fi
 }
 
-function prompt_russtone_precmd {
+function zle-line-init zle-keymap-select {
+  _prompt_russtone_vi_mode="${${KEYMAP/vicmd/%F{yellow}◼︎ %f/(main|viins}/%F{green}➜ %f}"
+  zle reset-prompt
+}
 
+# Preexec hook
+function prompt_russtone_preexec {
+  # Save timestamp before executing command
+  _prompt_russtone_timestamp_start=$(date +%s)
+}
+
+
+# Precmd hook
+function prompt_russtone_precmd {
   # Format PWD
   prompt_russtone_pwd
 
   # Format elapsed time
   prompt_russtone_elapsed_time
-
 }
 
+# Main
 function prompt_russtone_setup {
 
   # Load required functions
@@ -68,10 +77,12 @@ function prompt_russtone_setup {
   # Add hooks
   add-zsh-hook precmd prompt_russtone_precmd
   add-zsh-hook preexec prompt_russtone_preexec
+  zle -N zle-line-init
+  zle -N zle-keymap-select
 
   PROMPT='
 ${SSH_TTY:+"%F{red}%n%f%F{white}@%f%F{yellow}%M%f "}%F{blue}${_prompt_russtone_pwd}%f$(git_super_status)
-${editor_info[keymap]} '
+${_prompt_russtone_vi_mode} '
 
   RPROMPT='%F{yellow}${_prompt_russtone_elapsed_time}%f'
 
